a37abda2a64814693439ec3e7f33a38568d730d0
esciafardini <edward.ciafardini@aclaimant.com>
2022-08-12 10:49:48 -0400

    Chapter 4 progress

--- src/little_schemer_clj/chapter4.clj
+++ src/little_schemer_clj/chapter4.clj
@@ -1,12 +1,25 @@
 (ns little-schemer-clj.chapter4
   (:require
-   [clojure.inspector :refer [atom?]]))
+   [little-schemer-clj.chapter1 :refer [car cdr]]))
 
 ;; Numbers Games
 
-(atom? 1414)
 ;; All numbers are atoms
 ;; This book only onsiders non-negative integers (including 0)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;The First Commandment (first revision)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; When recurring on a list of atoms (lat),
+;;  Ask 2 questions about it:
+;;   1. (null? lat)
+;;   2. else
+;;
+;; When recurring on a number (n),
+;;  Ask 2 questions about it:
+;;   1. (zero? n)
+;;   2. else
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def add1 inc)
 
@@ -17,25 +30,38 @@
     (dec x)
     nil))
 
-(add1 4)
-(sub1 5)
-(sub1 1)
-(zero? 0)
-
 ;Recursive plus function using sub1, add1, zero?
 
-(defn plus
+(defn plus-from-book
   "Terminal condition is when a number reaches zero...
    Subtracting 1 from each until one of them reaches zero,
+   ping ponging btw sub1-ing x & sub1-ing y
    all the while wrapping in `add1` on each iteration"
   [x y]
   (if (zero? x)
     y
-    (add1 (plus y (sub1 x))))) ;=> "y" eventually reaches zero
+    (add1 (plus-from-book y (sub1 x))))) ;=> "y" eventually reaches zero
 
 ;almost like do this x + y times
-
 ;cons builds lists, add1 builds numbers
+
+(defn plus
+  "Keep It Simple, Schemer.
+   Rather than ping ponging between the two (and hurting my head) - 
+   just inc x times to y
+   recurses smaller # of times
+   Think of plus as cons for building numbers"
+  [x y]
+  (let [smaller (min x y)
+        bigger (max x y)]
+    (if (zero? smaller) bigger
+        (add1 (plus bigger (sub1 smaller))))))
+
+;ends up like thissss:
+;; (plus 4 7)
+;; (add1 (add1 (add1 (add1 7)
+(plus 4 7)
+(add1 (add1 (add1 (add1 7))))
 
 (defn minus
   "Subtraction: [minuend - subtrahend = difference]
@@ -46,6 +72,35 @@
     x
     (sub1 (minus x (sub1 y)))))
 
-;damn why is this so hard and weird to understand?
-;it's very "interesting" to read a book that looks like its for children
-;; that challenges my intelligence lol
+(defn tup?
+  "Let's define this recursively.
+   Check each element with int?
+   `tuple` in this context: list of numbers"
+  [coll]
+  (cond
+    (string? coll) false
+    (int? coll) false
+    (not (seq coll)) true
+    (int? (car coll)) (tup? (cdr coll))
+    :else false))
+
+(defn addtup
+  "My remix - before looking @ book's solution
+   Adds up all numbers in tup & returns single value"
+  [tup]
+  (when (tup? tup)
+    (if
+     (not (seq tup)) 0
+     (plus (car tup) (addtup (cdr tup))))))
+
+;TODO - move to test file
+(plus 3 (plus 3 4))
+(addtup [15 6 7 12 3])
+(addtup [99 33])
+(tup? [1 2 3 4])
+(tup? 3)
+(tup? "whattt")
+(tup? [1 2 3 4])
+
+(tup? [1 2 3 4 5])
+(cons (plus 1 2) (addtup [3 4 5]))
--- test/little_schemer_clj/chapter4_test.clj
+++ test/little_schemer_clj/chapter4_test.clj
@@ -1,28 +1,28 @@
 (ns little-schemer-clj.chapter4-test
   (:require
    [clojure.test :refer :all]
-   [little-schemer-clj.chapter4 :refer [add1 minus plus sub1]]))
+   [little-schemer-clj.chapter4 :refer [add1 minus plus plus-from-book sub1]]))
 
 (deftest plus-minus-equivalences
 
-  (is (= 7 (plus 2 5)))
-  (is (= 7 (add1 (plus 5 1))))
-  (is (= 7 (add1 (add1 (plus 1 4)))))
-  (is (= 7 (add1 (add1 (add1 (plus 4 0))))))
-  (is (= 7 (add1 (add1 (add1 (add1 (plus 0 3)))))))
+  (is (= 7 (plus-from-book 2 5)))
+  (is (= 7 (add1 (plus-from-book 5 1))))
+  (is (= 7 (add1 (add1 (plus-from-book 1 4)))))
+  (is (= 7 (add1 (add1 (add1 (plus-from-book 4 0))))))
+  (is (= 7 (add1 (add1 (add1 (add1 (plus-from-book 0 3)))))))
   ;terminal condition met
-  (is (= 3 (plus 0 3)))
+  (is (= 3 (plus-from-book 0 3)))
   (is (= 7 (add1 (add1 (add1 (add1 3))))))
 
-  (is (= 7 (plus 4 3)))
-  (is (= 7 (add1 (plus 3 3))))
-  (is (= 7 (add1 (add1 (plus 3 2)))))
-  (is (= 7 (add1 (add1 (add1 (plus 2 2))))))
-  (is (= 7 (add1 (add1 (add1 (add1 (plus 2 1)))))))
-  (is (= 7 (add1 (add1 (add1 (add1 (add1 (plus 1 1))))))))
-  (is (= 7 (add1 (add1 (add1 (add1 (add1 (add1 (plus 0 1)))))))))
+  (is (= 7 (plus-from-book 4 3)))
+  (is (= 7 (add1 (plus-from-book 3 3))))
+  (is (= 7 (add1 (add1 (plus-from-book 3 2)))))
+  (is (= 7 (add1 (add1 (add1 (plus-from-book 2 2))))))
+  (is (= 7 (add1 (add1 (add1 (add1 (plus-from-book 2 1)))))))
+  (is (= 7 (add1 (add1 (add1 (add1 (add1 (plus-from-book 1 1))))))))
+  (is (= 7 (add1 (add1 (add1 (add1 (add1 (add1 (plus-from-book 0 1)))))))))
   ;terminal condition met
-  (is (= 1 (plus 0 1)))
+  (is (= 1 (plus-from-book 0 1)))
   (is (= 7 (add1 (add1 (add1 (add1 (add1 (add1 1))))))))
 
   (is (= 7 (minus 10 3)))
@@ -31,4 +31,7 @@
   (is (= 7 (sub1 (sub1 (sub1 (minus 10 0))))))
   ;terminal condition met
   (is (= 10 (minus 10 0)))
-  (is (= 7 (sub1 (sub1 (sub1 10))))))
+  (is (= 7 (sub1 (sub1 (sub1 10)))))
+  
+  (is (= (plus 3 4) (plus-from-book 3 4)))
+  )
