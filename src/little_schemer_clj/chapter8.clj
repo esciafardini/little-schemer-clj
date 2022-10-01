28d11a72ad73be0f34893caef8c2c8afeab86940
esciafardini <edward.ciafardini@aclaimant.com>
2022-10-01 16:21:29 -0400

    Complete Chapter 8

--- src/little_schemer_clj/chapter8.clj
+++ src/little_schemer_clj/chapter8.clj
@@ -40,20 +40,12 @@
     ;kinda weird but ultimately just recursion
       (cons (first l) ((rember-f test-f) a (rest l))))))
 
-(def rember= (rember-f =))
-(rember= 5 [1 2 3 4 5 6 7 8])
-
 ;Currying
 (defn curried=
   "Currying is a function that returns a function with a passed-in argument
    Useful when not all arguments are provided right away at function call"
   [a]
   (fn [x] (= x a)))
-
-((curried= "yes") "yes")
-((curried= "no") "yes")
-
-(insertL "jeff" "daniel" ["hey" "my" "mans" "or" "were" "IT" "daniel" "?"])
 
 (defn insertL-f
   "Curry with the Sauce
@@ -72,10 +64,6 @@
       :else
       (cons (first lat) ((insertL-f f) nu old (rest lat))))))
 
-(def insertL-on-equal (insertL-f =))
-
-(insertL-on-equal "jeff" "daniel" ["hey" "my" "mans" "or" "were" "IT" "daniel" "?"])
-
 (defn insertR-f
   "more curry for the sauce
    to the right hand side"
@@ -90,26 +78,12 @@
       :else
       (cons (first l) ((insertR-f f) nu old (rest l))))))
 
-(def insertR-on-equal (insertR-f =))
-
-(insertR-on-equal "jeff" "daniel" ["hey" "my" "mans" "or" "were" "IT" "daniel" "?"])
-
 (defn insert-g-my-first-pass
   "inserts either at left or right depending on second argument"
   [f direction]
   (if (= direction :right)
     (insertR-f f)
     (insertL-f f)))
-
-(def insertRG-on-equal (insert-g-my-first-pass = :right))
-
-(insertRG-on-equal "jeff" "daniel" ["hey" "my" "mans" "or" "were" "IT" "daniel" "?"])
-
-(def insertLG-on-equal (insert-g-my-first-pass = :left))
-
-(insertLG-on-equal "jeff" "daniel" ["hey" "my" "mans" "or" "were" "IT" "daniel" "?"])
-
-; Book's version of insert-g
 
 (defn seqR
   "adds nu item to the right of old item"
@@ -137,40 +111,20 @@
       :else
       (cons (first l) ((insert-g seq-fn) nu old (rest l))))))
 
-(def insertL2 (insert-g seqL))
-(def insertR2 (insert-g seqR))
-
-(def insertL3 (insert-g (fn [nu old l] (cons nu (cons old l)))))
-(def insertR3 (insert-g (fn [nu old l] (cons old (cons nu l)))))
-
-(insertR3 "tenders" "chicken" ["I" "like" "chicken"])
-(insertL3 "buffalo" "chicken" ["I" "like" "chicken"])
-
-;RECALL subst
-(subst "carrot" "chicken" ["i" "eat" "chicken"])
-
-(defn seqS [nu old l]
+(defn seqS [nu _old l]
   (cons nu (rest l)))
 
 (def subst2 (insert-g seqS))
 (subst2 "carrot" "chicken" ["i" "eat" "chicken"])
 
-(defn seqrem [nu old l]
+(defn seqrem [_nu _old l]
   l)
 
 (defn yyy [a l]
   ((insert-g seqrem) false a l))
 
-(yyy "carrot" ["ok" "carrot" "now"])
-
 (defn yyyy [a l]
   ((insert-g (fn [_nu _old l] l)) 777 a l))
-
-(yyyy "carrot" ["forgive" "yesterday" "carrot" "idk"])
-
-;Makes sense - nu is always false or IRRELEVANT - whenever seqfn is called, its ignored
-; nu could be anything
-; when (= old first item), the list gets cons'd onto rest without it
 
 (defn atom-to-fn [x]
   (cond
@@ -191,12 +145,9 @@
     aexp
     ((atom-to-fn (first aexp)) (second aexp) (third aexp))))
 
-(value [* 4 5])
-
 (defn multi-rember-f
   "After all people come and people go away
    God with me He stays
-
    Pass in a function to remove items on"
   [test-f]
   (fn [a lat]
@@ -208,13 +159,6 @@
 
       :else
       (cons (first lat) ((multi-rember-f test-f) a (rest lat))))))
-
-(def remove-when-equals (multi-rember-f (fn [x y] (= x y))))
-(remove-when-equals "chicken" ["apples" "ginger" "chicken" "coconut" "chicken"])
-(remove-when-equals "salad" ["shrimp" "salad" "tuna" "salad" "and" "tuna"])
-
-(def remove-when-includes (multi-rember-f (fn [x y] (string/includes? y x))))
-(remove-when-includes "chicken" ["apples" "ginger" "chicken" "carrots" "coconut chicken" "chickens"])
 
 (defn multiremberT
   "takes a test-f and does the work on the lat"
@@ -231,8 +175,6 @@
 
 (defn equals-chicken? [x]
   (= x "chicken"))
-
-(multiremberT equals-chicken? ["potatoes" "chicken" "biscuit" "chicken" "korn"])
 
 (defn multirember&co
   "This will rember and then some
@@ -262,16 +204,6 @@
                        (cons (first lat) newlat)
                        removed-items)))))
 
-;& co stands for 'collector function'
-; the function is collecting two sequences
-
-;should be false
-(multirember&co "tuna" ["strawberries" "tuna" "and" "swordfish"] (fn [x y] (not (seq y))))
-
-(multirember&co 'tuna ['strawberries 'with 'tuna 'cream] (fn [newlat removed-items] (str (string/join " " newlat) " is better without " (apply str removed-items))))
-(multirember&co 'tuna ['strawberries 'in 'tuna 'with 'tuna 'cream] (fn [newlat removed-items] (println newlat) (println removed-items)))
-(multirember&co 'tuna ['strawberries 'in 'tuna 'with 'tuna 'cream] (fn [newlat removed-items] (println (str "Your new list is: " newlat)) (println (str "Removed " (count removed-items) " instances of ") (first removed-items))))
-
 (defn multiinsertLR
   "will insert nu to the left of oldL and to the right of oldR"
   [nu oldL oldR lat]
@@ -286,8 +218,6 @@
 
     :else
     (cons (first lat) (multiinsertLR nu oldL oldR (rest lat)))))
-
-(multiinsertLR "fried" "chicken" "deep" ["we" "ggonnnaa" "eat" "chicken" "and" "deep" "oreos"])
 
 (defn multiinsertLR&co
   "builds a new collection and counts the number of L inserts + R inserts separately"
@@ -308,7 +238,7 @@
     (multiinsertLR&co nu oldL oldR (rest lat)
                       (fn [new-lat left-insertions right-insertions]
                         (f
-                         (cons oldR (cons nu new-lat))
+                         (cons (first lat) (cons nu new-lat))
                          left-insertions
                          (inc right-insertions))))
 
@@ -327,3 +257,51 @@
    (println x)
    (println y)
    (println z)))
+
+(defn evens-only* [lat]
+  (cond
+    (not (seq lat)) '()
+
+    (not (coll? (first lat)))
+    (if (odd? (first lat))
+
+      (evens-only* (rest lat))
+      (cons (first lat) (evens-only* (rest lat))))
+
+    :else
+    (cons (evens-only* (first lat)) (evens-only* (rest lat)))))
+
+(defn evens-only*&co
+  "Combines the concepts of * functions (handle nested collections)
+   and &co functions where collectors are making lists & return values
+   as the function recurses.
+
+   I do not understand this fully.  Need to take some time with it."
+  [lat f]
+  (let [idx0 (first lat)
+        remaining (rest lat)]
+    (cond
+      (not (seq lat))
+      (f lat 1 0)
+
+      ;; a singular atom at index 0!
+      (not (coll? idx0))
+      (if (even? idx0)
+        ;; EVEN!
+        (evens-only*&co remaining
+                        (fn [new-lat evens-product odds-sum]
+                          (f (cons idx0 new-lat) (* idx0 evens-product) odds-sum)))
+
+        ;; ODD!
+        (evens-only*&co remaining
+                        (fn [new-lat evens-product odds-sum]
+                          (f new-lat evens-product (+ idx0 odds-sum)))))
+
+      :else
+      ;; a collection at index 0!
+      (evens-only*&co idx0
+                      (fn [nl ep os]
+                        (evens-only*&co remaining
+                                        (fn [new-lat evens-product odds-sum]
+                                          (f (cons nl new-lat) (* ep evens-product) (+ os odds-sum)))))))))
+
--- test/little_schemer_clj/chapter8_test.clj
+++ test/little_schemer_clj/chapter8_test.clj
@@ -1,3 +1,65 @@
-(ns little-schemer-clj.chapter8-test)
+(ns little-schemer-clj.chapter8-test
+  (:require
+   [clojure.string :as string]
+   [little-schemer-clj.chapter3 :refer [insertL subst]]
+   [little-schemer-clj.chapter8 :refer [curried= equals-chicken? evens-only*
+                                        evens-only*&co insert-g
+                                        insert-g-my-first-pass insertL-f insertR-f multi-rember-f multiinsertLR multirember&co
+                                        multiremberT rember-f seqL seqR value yyy yyyy]]))
 
+(def rember= (rember-f =))
+(rember= 5 [1 2 3 4 5 6 7 8])
+((curried= "yes") "yes")
+((curried= "no") "yes")
 
+(insertL "jeff" "daniel" ["hey" "my" "mans" "or" "were" "IT" "daniel" "?"])
+
+(def insertL-on-equal (insertL-f =))
+
+(insertL-on-equal "jeff" "daniel" ["hey" "my" "mans" "or" "were" "IT" "daniel" "?"])
+(def insertR-on-equal (insertR-f =))
+
+(insertR-on-equal "jeff" "daniel" ["hey" "my" "mans" "or" "were" "IT" "daniel" "?"])
+(def insertRG-on-equal (insert-g-my-first-pass = :right))
+
+(insertRG-on-equal "jeff" "daniel" ["hey" "my" "mans" "or" "were" "IT" "daniel" "?"])
+
+(def insertLG-on-equal (insert-g-my-first-pass = :left))
+
+(insertLG-on-equal "jeff" "daniel" ["hey" "my" "mans" "or" "were" "IT" "daniel" "?"])
+(def insertL2 (insert-g seqL))
+(def insertR2 (insert-g seqR))
+
+(def insertL3 (insert-g (fn [nu old l] (cons nu (cons old l)))))
+(def insertR3 (insert-g (fn [nu old l] (cons old (cons nu l)))))
+
+(insertR3 "tenders" "chicken" ["I" "like" "chicken"])
+(insertL3 "buffalo" "chicken" ["I" "like" "chicken"])
+
+;RECALL subst
+(subst "carrot" "chicken" ["i" "eat" "chicken"])
+(yyy "carrot" ["ok" "carrot" "now"])
+(yyyy "carrot" ["forgive" "yesterday" "carrot" "idk"])
+
+(value [* 4 5])
+(def remove-when-equals (multi-rember-f (fn [x y] (= x y))))
+(remove-when-equals "chicken" ["apples" "ginger" "chicken" "coconut" "chicken"])
+(remove-when-equals "salad" ["shrimp" "salad" "tuna" "salad" "and" "tuna"])
+
+(def remove-when-includes (multi-rember-f (fn [x y] (string/includes? y x))))
+(remove-when-includes "chicken" ["apples" "ginger" "chicken" "carrots" "coconut chicken" "chickens"])
+(multiremberT equals-chicken? ["potatoes" "chicken" "biscuit" "chicken" "korn"])
+(multirember&co "tuna" ["strawberries" "tuna" "and" "swordfish"] (fn [x y] (not (seq y))))
+
+(multirember&co 'tuna ['strawberries 'with 'tuna 'cream] (fn [newlat removed-items] (str (string/join " " newlat) " is better without " (apply str removed-items))))
+(multirember&co 'tuna ['strawberries 'in 'tuna 'with 'tuna 'cream] (fn [newlat removed-items] (println newlat) (println removed-items)))
+(multirember&co 'tuna ['strawberries 'in 'tuna 'with 'tuna 'cream] (fn [newlat removed-items] (println (str "Your new list is: " newlat)) (println (str "Removed " (count removed-items) " instances of ") (first removed-items))))
+
+(multiinsertLR "fried" "chicken" "deep" ["we" "ggonnnaa" "eat" "chicken" "and" "deep" "oreos"])
+
+(evens-only* [1 2 3 4 [4 5 6 [7 8 9 [5 4 3] 4 3]]])
+
+(evens-only*&co [1 2 [1 2 3 [4 3 5 [6]]] 3 4] (fn [x y z]
+                                                (println x)
+                                                (println y)
+                                                (println z)))
